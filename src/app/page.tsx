09920a4b96cb7ef634dc1f8dd9841eca21c89b57
stianjsu <stianjsu@gmail.com>
2023-04-08 13:28:13 +0200

    Initial commit Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="m-auto text-6xl font-bold">HELLO WORLD</p>
+      </div>
+    </main>
+  );
+}
