bc4e0f2764361d7178d79f7486fd968c574fde78
stianjsu <stianjsu@gmail.com>
2023-04-16 23:22:09 +0200

    Adds pwa support

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const nextPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextPWA(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,32 @@
 import "./globals.css";
+import type { Metadata } from "next";
 
-export const metadata = {
-  title: "My Transfer",
-  description: "Upload your files to access them accross devices",
+const APP_NAME = "My Transfer";
+const APP_DEFAULT_TITLE = "My Transfer";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "Transfer your files accross devices";
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  themeColor: "#1e293b",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  icons: {
+    shortcut: "/favicon.ico",
+    apple: [{ url: "/icons/apple-touch-icon.png", sizes: "180x180" }],
+  },
 };
 
 export default function RootLayout({
