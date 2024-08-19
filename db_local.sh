5eeab3fbeb0339ec4b93402ce24c54b0d5b188b6
stianjsu <stianjsu@gmail.com>
2024-08-19 15:23:41 +0200

    update scripts and config for migrations

--- db_local.sh
+++ db_local.sh
@@ -2,7 +2,7 @@
 set -a
 source ./.env.local
 
-while getopts 'sp' opt; do
+while getopts 'spmg' opt; do
   case "$opt" in
     s)
       npm run db:studio
@@ -12,10 +12,21 @@ while getopts 'sp' opt; do
       npm run db:push
       exit 1
       ;;
-    
+    m)
+      
+      npm run db:migrate
+      exit 1
+      ;;
+    g)
+      read -p "name for migration: " MIG_NAME
+      npm run db:generate -- --name=$MIG_NAME
+      exit 1
+      ;;
   esac
 done
 
 echo "Missing input"
 echo "  -s: start drizzle_studio"
 echo "  -p: push db changes"
+echo "  -m: migrate db"
+echo "  -g: generate db migration"
