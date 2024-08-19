ee812c1033e437700aba05bb9b5658bf535b166a
stianjsu <stianjsu@gmail.com>
2024-08-20 00:48:45 +0200

    adds clerk and route protection

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import { SignedIn, UserButton } from "@clerk/nextjs"
 
 export default function Navbar() {
   return (
@@ -22,9 +23,11 @@ export default function Navbar() {
           </Link>
         </div>
 
-        <div className="flex items-center">
-          <button className="h-full w-24 hover:font-bold">Sign out</button>
-        </div>
+        <SignedIn>
+          <div className="flex h-full w-24 items-center justify-center">
+            <UserButton />
+          </div>
+        </SignedIn>
       </div>
     </nav>
   )
--- src/env.js
+++ src/env.js
@@ -2,10 +2,6 @@ import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
 
 export const env = createEnv({
-  /**
-   * Specify your server-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars.
-   */
   server: {
     DATABASE_URL: z.string().url(),
     NODE_ENV: z
@@ -20,22 +16,16 @@ export const env = createEnv({
       .enum(["development", "preview", "production"])
       .default("development"),
     UPLOADTHING_SECRET: z.string(),
+    CLERK_SECRET_KEY: z.string(),
   },
 
-  /**
-   * Specify your client-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars. To expose them to the client, prefix them with
-   * `NEXT_PUBLIC_`.
-   */
   client: {
     NEXT_PUBLIC_UPLOADTHING_APP_ID: z.string(),
-    // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string(),
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string(),
   },
 
-  /**
-   * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
-   * middlewares) or client-side so we need to destruct manually.
-   */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
@@ -47,16 +37,15 @@ export const env = createEnv({
     VERCEL_ENV: process.env.VERCEL_ENV,
     UPLOADTHING_SECRET: process.env.UPLOADTHING_SECRET,
     NEXT_PUBLIC_UPLOADTHING_APP_ID: process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
-  /**
-   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
-   * useful for Docker builds.
-   */
+
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
-  /**
-   * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
-   * `SOME_VAR=''` will throw an error.
-   */
+
   emptyStringAsUndefined: true,
 })
