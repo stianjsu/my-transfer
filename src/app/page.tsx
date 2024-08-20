2c2da375779158776e1b253363204cb2a08aec20
stianjsu <stianjsu@gmail.com>
2024-08-20 11:17:35 +0200

    link auth to file upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,16 @@ import { FileDisplay } from "@/components/FilesDisplay"
 import { db } from "../server/db"
 import { FILE_URL } from "@/components/uploadthing"
 import DropZone from "@/components/Dropzone"
+import { auth } from "@clerk/nextjs/server"
 
 const Home = async () => {
-  const uplodedFiles = await db.query.uploadedFilesTable.findMany()
+  const session = auth()
+
+  if (!session || !session.userId) return session.redirectToSignIn()
+
+  const uplodedFiles = await db.query.uploadedFilesTable.findMany({
+    where: (files, { eq }) => eq(files.userId, session.userId),
+  })
 
   return (
     <main className="m-auto flex min-h-screen flex-col gap-4 pt-24">
