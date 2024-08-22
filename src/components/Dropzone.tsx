7570fdb1b7a63bdccb2b481d6c52041686bf4a91
stianjsu <stianjsu@gmail.com>
2024-08-22 15:00:46 +0200

    adds dropzone styling and analytics

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -3,21 +3,32 @@
 import { UploadDropzone } from "@/components/uploadthing"
 import { useRouter } from "next/navigation"
 import { toast } from "sonner"
+import { RefreshIcon } from "./Icons"
 
 export function DropZone() {
   const router = useRouter()
 
   return (
-    <UploadDropzone
-      className="bg-slate-50"
-      endpoint="fileUploader"
-      onClientUploadComplete={() => {
-        // Do something with the response
-        router.refresh()
-      }}
-      onUploadError={(error: Error) => {
-        toast.error(error.message)
-      }}
-    />
+    <>
+      <UploadDropzone
+        className="flex gap-1 bg-slate-50 p-4 ut-button:h-8 ut-button:max-w-36 ut-button:bg-slate-700 ut-button:text-sm ut-label:text-slate-700"
+        endpoint="fileUploader"
+        onClientUploadComplete={() => {
+          router.refresh()
+        }}
+        onUploadError={(error: Error) => {
+          toast.error(error.message)
+        }}
+      />
+      <div className="flex w-full items-center justify-end gap-2 align-middle">
+        <span>Refresh</span>
+        <div
+          className="mr-6 cursor-pointer rounded-lg bg-slate-900 p-2 hover:bg-slate-700"
+          onClick={router.refresh}
+        >
+          <RefreshIcon size={28} />
+        </div>
+      </div>
+    </>
   )
 }
