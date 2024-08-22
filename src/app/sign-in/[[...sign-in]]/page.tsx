95330ee88b2f72aa417729b27c3dcab56bcb9f94
stianjsu <stianjsu@gmail.com>
2024-08-22 12:34:29 +0200

    clean up route transition after sign out

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs"
 export default function SignInPage() {
   return (
     <main className="m-auto flex min-h-screen flex-col items-center gap-4 pt-24">
-      <SignIn signUpUrl={"/sign-up"} />
+      <SignIn path="/sign-in" />
     </main>
   )
 }
